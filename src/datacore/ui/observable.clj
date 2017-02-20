2d35e49111e373e6693f2620cefe4c2ab76d29ea
Stathis Sideris <stathis@pixelated-noise.com>
2017-02-20 03:53:04 +0200

    Better handling of ListChangeListener

--- src/datacore/ui/observable.clj
+++ src/datacore/ui/observable.clj
@@ -15,22 +15,23 @@
                      (partition-by first))
         idx     (atom -1)
         current #(nth diff @idx)]
-    (dev/trace-proxy
-     (proxy [ListChangeListener$Change] [the-list]
-       (next [] (swap! idx inc) (< @idx (count diff)))
-       (reset [] (reset! idx 0))
+    (proxy [ListChangeListener$Change] [the-list]
+      (next [] (swap! idx inc) (< @idx (count diff)))
+      (reset [] (reset! idx 0))
 
-       (wasAdded [] (some? (#{:add :edit} (ffirst (current)))))
-       (wasRemoved [] (some? (#{:delete :edit} (ffirst (current)))))
-       (wasUpdated [] (= :edit (ffirst (current))))
-       (wasPermutated [] false)
+      (wasAdded [] (some? (#{:add :edit} (ffirst (current)))))
+      (wasRemoved [] (some? (#{:delete :edit} (ffirst (current)))))
+      (wasUpdated [] (= :edit (ffirst (current))))
+      (wasPermutated [] false)
 
-       (getFrom [] (apply + (map count (take @idx diff))))
-       (getTo [] (+ (apply + (map count (take @idx diff)))
-                    (count (current))))
+      (getFrom [] (apply + (map count (take @idx diff))))
+      (getTo [] (+ (apply + (map count (take @idx diff)))
+                   (count (current))))
 
-       (getRemoved [] (if-not (.wasRemoved this) [] (map second (current))))))))
+      (getRemoved [] (if-not (.wasRemoved this) [] (map second (current)))))))
 
+(def atom-observable-list-id (atom -1))
+(def atom-observable-registry (atom {}))
 (defmethod observable-list clojure.lang.Atom
   [x]
   (proxy [Object ObservableList] []
@@ -40,7 +41,11 @@
     (forEach [consumer] (.forEach @x consumer))
     (isEmpty [] (empty? @x))
     (setAll [coll] (reset! x coll))
-    (addListener [listener]
-      (add-watch x (str "observable-list-atom-listener" (rand-int Integer/MAX_VALUE))
-                 (fn [k ref old new]
-                   (.onChanged listener (list-change this old new)))))))
+    (addListener [^ListChangeListener listener]
+      (let [id (str "observable-list-atom-listener" (swap! atom-observable-list-id inc))]
+        (swap! atom-observable-registry {listener id})
+        (add-watch x id (fn [k ref old new]
+                          (.onChanged listener (list-change this old new))))))
+    (removeListener [^ListChangeListener listener]
+      (when-let [id (get @atom-observable-registry listener)]
+        (remove-watch x id)))))
