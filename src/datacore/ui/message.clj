4c7b63be378cc772366b9e81710f4e2fc25ac0f4
Stathis Sideris <stathis@pixelated-noise.com>
2017-03-04 11:02:10 +0200

    Make current-message read only by making it a fomrula cell

--- src/datacore/ui/message.clj
+++ src/datacore/ui/message.clj
@@ -1,10 +1,11 @@
 (ns datacore.ui.message
-  (:require [datacore.cells :as c :refer [cell]]))
+  (:require [datacore.cells :as c :refer [cell cell=]]))
 
-(def current-message (cell "Welcome"))
+(def ^:private current-message-input (cell "Welcome"))
+(def current-message (cell= @current-message-input))
 
 (defn message [x]
-  (c/swap! current-message (fn [_] x)))
+  (c/swap! current-message-input (fn [_] x)))
 
 (defn error [x]
-  (c/swap! current-message (fn [_] x)))
+  (c/swap! current-message-input (fn [_] x)))
