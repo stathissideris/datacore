b163c26d42eae5f8a31952f1c4b598da1f8c1166
Stathis Sideris <stathis@pixelated-noise.com>
2017-03-26 00:29:22 +0200

    Add default exception handler

---
+++ src/datacore/main.clj
@@ -0,0 +1,24 @@
+(ns datacore.main
+  (:require [clojure.repl :refer [pst]]))
+
+(defmacro with-err-str
+  "Evaluates exprs in a context in which *err* is bound to a fresh
+  StringWriter.  Returns the string created by any nested printing
+  calls."
+  {:added "1.0"}
+  [& body]
+  `(let [s# (new java.io.StringWriter)]
+     (binding [*err* s#]
+       ~@body
+       (str s#))))
+
+(defn- global-exception-handler []
+  (reify Thread$UncaughtExceptionHandler
+    (uncaughtException [this thread throwable]
+      (let [trace (with-err-str (pst throwable 150))]
+        (println "UNCAUGHT EXCEPTION" trace)))))
+
+(defn init []
+  (Thread/setDefaultUncaughtExceptionHandler (global-exception-handler)))
+
+(init)
