5453b3f66613d6eca3beee65bfae8ac79849156d
Stathis Sideris <stathis@pixelated-noise.com>
2017-04-04 01:16:02 +0300

    More javafx maps

--- test/datacore/ui/java_fx_test.clj
+++ test/datacore/ui/java_fx_test.clj
@@ -31,8 +31,15 @@
     (is (false? (has-style-class? c "baz")))))
 
 (deftest set-fields-test
-  (let [l (label "foo")]
-    (set-fields! l {:id          "the-id"
-                    :style-class ["focusable"]})
-    (is (= "the-id" (.getId l)))
-    (is (= ["focusable"] (seq (.getStyleClass l))))))
+  (testing "with maps"
+    (let [l (label "foo")]
+      (set-fields! l {:id          "the-id"
+                      :style-class ["focusable"]})
+      (is (= "the-id" (.getId l)))
+      (is (= ["focusable"] (seq (.getStyleClass l))))))
+  (testing "with vectors of vectors"
+    (let [l (label "foo")]
+      (set-fields! l [[:id "the-id"]
+                      [:style-class ["focusable"]]])
+      (is (= "the-id" (.getId l)))
+      (is (= ["focusable"] (seq (.getStyleClass l)))))))
