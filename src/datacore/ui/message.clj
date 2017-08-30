1fdd124ac3e460139ec068ee1caa0511f9d88814
Stathis Sideris <stathis@pixelated-noise.com>
2017-08-30 04:44:02 +0300

    kludge for cells table slowness

--- src/datacore/ui/message.clj
+++ src/datacore/ui/message.clj
@@ -2,6 +2,8 @@
   (:require [datacore.cells :as c]
             [datacore.ui.timer :as timer]))
 
+(def message-timeout 3) ;;TODO normally 3000, but causes slowness for cells table
+
 (def initial-state
   {:type :message
    :msg  "Ready"})
@@ -19,10 +21,10 @@
   (c/reset! message-input {:type :message
                            :msg  (str x)})
   (when-let [t @timer] (timer/cancel t))
-  (reset! timer (timer/delayed 3000 #(c/reset! message-input initial-state))))
+  (reset! timer (timer/delayed message-timeout #(c/reset! message-input initial-state))))
 
 (defn error [x]
   (c/reset! message-input {:type :error
                            :msg  (str x)})
   (when-let [t @timer] (timer/cancel t))
-  (reset! timer (timer/delayed 3000 #(c/reset! message-input initial-state))))
+  (reset! timer (timer/delayed message-timeout #(c/reset! message-input initial-state))))
